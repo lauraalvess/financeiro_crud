d32a94e0b9fe625b531fd1d6988a3193660ff565
lauraalvess <laurab.alves@gmail.com>
2021-09-09 22:20:22 -0300

    persistência local - respostas das questões estão comentadas no código main.dart

---
+++ lib/app/database/script.dart
@@ -0,0 +1,31 @@
+final createTable = '''
+  CREATE TABLE despesa(
+    id INT NOT NULL PRIMARY KEY
+    ,nome VARCHAR(200) NOT NULL
+    ,tipo CHAR(16) NOT NULL
+    ,valor FLOAT NOT NULL
+    ,url_avatar VARCHAR(300) NOT NULL
+  )
+''';
+
+final insert1 = '''
+INSERT INTO des (nome, tipo, valor, url_avatar)
+VALUES
+'Mercado XY', 'alimentação', '123.30', 'https://cdn.pixabay.com/photo/2015/05/13/07/46/burger-765125__480.jpg'
+''';
+
+
+final insert2 = '''
+INSERT INTO despesa (nome, tipo, valor, url_avatar)
+VALUES 
+ 'Ifood', 'alimentação', '20.40', 'https://cdn.pixabay.com/photo/2015/05/13/07/46/burger-765125__480.jpg'
+
+''';
+
+
+final insert3 = '''
+INSERT INTO despesa (nome, tipo, valor, url_avatar)
+VALUES 'Internet', 'residência', '100.00', 'https://cdn.pixabay.com/photo/2014/04/03/00/38/house-308936__340.png'
+''';
+
+ 
--- lib/app/view/despesa_list.dart
+++ lib/app/view/despesa_list.dart
@@ -2,7 +2,13 @@ import 'package:financeiro_crud/app/my_app.dart';
 import 'package:flutter/material.dart';
 
 class DespesaList extends StatelessWidget {
-  const DespesaList({ Key? key }) : super(key: key);
+  //const DespesaList({ Key? key }) : super(key: key);
+
+  final lista =[
+    {'nome':'Ana', 'telefone':'17 9999-9999', 'avatar':'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807__480.png'},
+    {'nome': 'Andre', 'telefone': '18 9999-9999', 'avatar':'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331__480.png'},
+    {'nome': 'Beatriz', 'telefone': '19 9999-9999', 'avatar':'https://cdn.pixabay.com/photo/2014/04/02/14/10/female-306407__480.png'}
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +23,29 @@ class DespesaList extends StatelessWidget {
             }), 
         ],
       ),
-      body: ListView(),
+       body: ListView.builder(
+        itemCount: lista.length,
+        itemBuilder: (context, i){
+          var contato = lista[i];
+          var avatar = CircleAvatar(backgroundImage: NetworkImage(contato['avatar'] ?? ""));
+
+          //return Text(contato['nome'] ?? "");
+          return ListTile(
+            title: Text(contato['nome'] ?? ""),
+            subtitle: Text(contato['telefone'] ?? ""),
+            leading: avatar,
+            trailing: Container(
+              width: 100,
+              child: Row(
+                children: [
+                  IconButton(onPressed: null, icon:Icon(Icons.edit)),
+                  IconButton(onPressed: null, icon:Icon(Icons.delete))
+                ],
+              )
+            ),
+          );
+        }
+      ),
     );
   }
 }
